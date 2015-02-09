b43026feb2f689e0972027b6db358524f81e21d4
Jonathan Weber <jonathan.weber@univ-lorraine.fr>
2015-02-09 09:06:05 +0100

    -Modify Neighbourhood2D

--- src/main/java/fr/unistra/pelican/util/neighbourhood/Neighbourhood2D.java
+++ src/main/java/fr/unistra/pelican/util/neighbourhood/Neighbourhood2D.java
@@ -20,6 +20,28 @@ public class Neighbourhood2D {
 	}
 	
 	/**
+	 * Returns the point of the backward semi 4-neighbourhood
+	 * (-1,0),(0,-1) 
+	 * @return backward semi 4-neighbourhood
+	 */
+	public static Point[] getBackwardSemi4Neighboorhood()
+	{
+		Point[] neighbourhood = {new Point(-1,0),new Point(0,-1)};
+		return neighbourhood;
+	}
+	
+	/**
+	 * Returns the point of the forward semi 4-neighbourhood
+	 * (1,0),(0,1)
+	 * @return forward semi 4-neighbourhood
+	 */
+	public static Point[] getForwardSemi4Neighboorhood()
+	{
+		Point[] neighbourhood = {new Point(1,0),new Point(0,1)};
+		return neighbourhood;
+	}
+	
+	/**
 	 * Returns the point of the 8-neighbourhood
 	 * (-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)
 	 * @return 8-neighbourhood
